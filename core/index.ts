90fa05895f102174a497b1494daa09667308c464
yxb123456 <2186256471@qq.com>
2025-09-07 17:59:14 +0800

    feat(ssh): :sparkles: 使用node-ssh 来完成ssh链接测试

--- core/index.ts
+++ core/index.ts
@@ -5,8 +5,11 @@ import path from 'node:path'
 import process from 'node:process'
 import { promisify } from 'node:util'
 import archiver from 'archiver'
-
+import { NodeSSH } from 'node-ssh'
+// 现在已经可以成功生成部署日志  ZIP文件(包含前端项目打包生成的dist目录以及DockerFile);
 const appendFile = promisify(fs.appendFile)
+// 创建SSH实例
+const ssh = new NodeSSH()
 // Vite-plugin-docker-deploy核心文件
 export default function dockerDeploy(options: DockerDeployOptions):
 Plugin {
@@ -46,6 +49,16 @@ Plugin {
         archive.on('error', reject)
       })
       await log(`📦 打包 dist + Dockerfile 完成 -> ${zipPath}`)
+      // 先实现单服务器部署; 现 使用本地虚拟机进行测试;
+      ssh.connect({
+        host: '192.168.136.130',
+        port: 22,
+        password: '123456',
+      })
+        .then(async () => {
+          const dockerVersionResult = await ssh.execCommand('docker version')
+          console.warn('🐳 docker version:', dockerVersionResult.stdout)
+        })
     },
   }
 }
