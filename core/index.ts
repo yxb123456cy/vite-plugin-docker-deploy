a9939e86d67ed9857eb868c89a9088a60c125ba7
yxb123456 <2186256471@qq.com>
2025-09-07 16:29:51 +0800

    feat(deploy): :sparkles: 添加NPM发布配置相关

--- core/index.ts
+++ core/index.ts
@@ -1,1 +1,18 @@
+import type { Plugin } from 'vite'
+import type { DockerDeployOptions } from './type'
 // Vite-plugin-docker-deploy核心文件
+export default function dockerDeploy(options: DockerDeployOptions):
+Plugin {
+  return {
+    name: 'vite-plugin-docker-deploy',
+    apply: 'build',
+
+    async closeBundle() {
+      const envConfig = options.envs[options.targetEnv]
+      if (!envConfig) {
+        throw new Error(`未找到环境配置: ${options.targetEnv}`)
+      }
+      console.warn(`🚀 开始部署环境: ${options.targetEnv}`)
+    },
+  }
+}
